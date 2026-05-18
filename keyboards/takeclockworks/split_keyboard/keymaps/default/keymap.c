e1eab4e3810952404aec40c7c9cb757cdab31265
takehiko-cmd <takehiko_777jp@yahoo.co.jp>
2026-05-18 20:09:57 +0900

    ファンクションキーの処理を追加し、レイヤー管理を強化。キーマップの一部を修正。

--- keyboards/takeclockworks/split_keyboard/keymaps/default/keymap.c
+++ keyboards/takeclockworks/split_keyboard/keymaps/default/keymap.c
@@ -13,6 +13,78 @@ enum layers {
     _FN3,
 };
 
+enum custom_keycodes {
+    FN1_F21 = SAFE_RANGE,
+    FN2_F22,
+    FN3_F23,
+};
+
+#define FN1_F21_DELAY_MS 500
+
+static uint16_t fn1_timer;
+static uint8_t fn1_press_count;
+static bool fn1_f21_registered;
+
+static void handle_fn_key(uint16_t keycode, bool pressed) {
+    switch (keycode) {
+        case FN1_F21:
+            if (pressed) {
+                if (fn1_press_count == 0) {
+                    fn1_timer = timer_read();
+                    fn1_f21_registered = false;
+                }
+                fn1_press_count++;
+                layer_on(_FN1);
+            } else {
+                if (fn1_press_count > 0) {
+                    fn1_press_count--;
+                }
+                if (fn1_press_count == 0) {
+                    if (fn1_f21_registered) {
+                        unregister_code(KC_F21);
+                        fn1_f21_registered = false;
+                    }
+                    layer_off(_FN1);
+                }
+            }
+            break;
+        case FN2_F22:
+            if (pressed) {
+                tap_code(KC_F22);
+                layer_on(_FN2);
+            } else {
+                layer_off(_FN2);
+            }
+            break;
+        case FN3_F23:
+            if (pressed) {
+                tap_code(KC_F23);
+                layer_on(_FN3);
+            } else {
+                layer_off(_FN3);
+            }
+            break;
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case FN1_F21:
+        case FN2_F22:
+        case FN3_F23:
+            handle_fn_key(keycode, record->event.pressed);
+            return false;
+    }
+    return true;
+}
+
+void matrix_scan_user(void) {
+    if (fn1_press_count > 0 && !fn1_f21_registered && timer_elapsed(fn1_timer) >= FN1_F21_DELAY_MS) {
+        register_code(KC_F21);
+        fn1_f21_registered = true;
+    }
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /*
@@ -23,16 +95,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_BASE] = {
     // Row0
-    { KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    JP_ZKHK,  KC_INS,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_SLSH },
+    { KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    JP_ZKHK,  KC_BSPC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_SLSH },
 
     // Row1
-    { KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LGUI,  KC_PSCR, KC_H,    KC_J,    KC_K,    KC_L,    JP_MINS, JP_PLUS },
+    { KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LGUI,  KC_INS, KC_H,    KC_J,    KC_K,    KC_L,    JP_MINS, JP_PLUS },
 
     // Row2
-    { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,   KC_BSPC, KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_UNDS, KC_RSFT },
+    { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,   KC_PSCR, KC_N,    KC_M,    JP_COMM, JP_DOT,  JP_UNDS, KC_RSFT },
 
     // Row3
-    { KC_LCTL, KC_LALT, MO(_FN3), MO(_FN2), MO(_FN1), KC_SPC, KC_ENT,  KC_ENT,  KC_SPC,  MO(_FN1), MO(_FN2), MO(_FN3), KC_RALT, KC_RCTL }
+    { KC_LCTL, KC_LALT, FN3_F23,  FN2_F22,  FN1_F21,  KC_SPC, KC_ENT,  KC_ENT,  KC_SPC,  FN1_F21,  FN2_F22,  FN3_F23,  KC_RALT, KC_RCTL }
 },
 
 /*
