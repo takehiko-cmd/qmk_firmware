d4b449df21dbe840efb86378f455c9dd01cdeaed
takehiko-cmd <takehiko_777jp@yahoo.co.jp>
2026-05-22 07:51:53 +0900

    FN2レイヤーのJP_MINSをJP_UNDSに変更し、FN2_MIRRORレイヤーでも同様の変更を適用

--- keyboards/takeclockworks/split_keyboard/keymaps/default/keymap.c
+++ keyboards/takeclockworks/split_keyboard/keymaps/default/keymap.c
@@ -366,7 +366,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_FN2] = {
     // Row0
-    { JP_ZKHK, KC_ESC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   BOOT_HOLD, JP_MINS, JP_COLN, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSPC },
+    { JP_ZKHK, KC_ESC,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   BOOT_HOLD, JP_UNDS, JP_COLN, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSPC },
 
     // Row1
     { KC_PSCR, KC_TAB,  JP_EXLM, JP_DQUO, JP_HASH, JP_DLR,  JP_PERC,  JP_YEN,  JP_TILD, JP_CIRC, JP_LPRN, JP_RPRN, KC_INS,  KC_DEL  },
@@ -418,7 +418,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_FN2_MIRROR] = {
     // Row0
-    { KC_BSPC, KC_NO,   KC_NO,   KC_NO,   KC_NO,   JP_COLN, JP_MINS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO   },
+    { KC_BSPC, KC_NO,   KC_NO,   KC_NO,   KC_NO,   JP_COLN, JP_UNDS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO   },
 
     // Row1
     { KC_DEL,  KC_INS,  JP_RPRN, JP_LPRN, JP_CIRC, JP_TILD, JP_YEN,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO   },
